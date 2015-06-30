5a6d74dcaea8b38d879ee2c91c986e1b79c6cbed
bairdweng <Baird@BairdsMac-Pro.local>
2015-06-30 14:53:42 +0800

    1.0
    新swift项目

---
+++ TXBBM/TXBBM/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  TXBBM
+//
+//  Created by Baird-weng on 15/6/30.
+//  Copyright (c) 2015年 Baird-weng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+  
+    
+}
+
